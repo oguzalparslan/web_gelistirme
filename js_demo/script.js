4bf9ef319e4417459644c7973bf66559ceb4b96c
oguzalparslan <77553495+oguzalparslan@users.noreply.github.com>
2022-03-24 23:08:02 +0300

    web sayfası

--- js_demo/script.js
+++ js_demo/script.js
@@ -2,15 +2,17 @@
 
 function myFunc() {
     document.getElementById("popup-menu").style.display = "none";
-    document.getElementById("popup-page").style.display="inline";
+    document.getElementById("popup-page").style.opacity ="1";
+
+
 }
 function close() {
-    document.getElementById("popup-page").style.display = "none";
+    document.getElementById("popup-page").style.opacity = "0";
 
 }
-
-
-
 function a() {
     alert("Giriş Yapılıyor")
+}
+function b() {
+    alert("Kayıt sayfasına yönlendiriliyor")
 }
