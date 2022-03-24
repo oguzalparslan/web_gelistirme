803146dcf34fb64863544953982499d5579df4ab
oguzalparslan <77553495+oguzalparslan@users.noreply.github.com>
2022-03-24 16:04:13 +0300

    popup
    popup denemem

---
+++ js_demo/script.js
@@ -0,0 +1,16 @@
+
+
+function myFunc() {
+    document.getElementById("popup-menu").style.display = "none";
+    document.getElementById("popup-page").style.display="inline";
+}
+function close() {
+    document.getElementById("popup-page").style.display = "none";
+
+}
+
+
+
+function a() {
+    alert("Giriş Yapılıyor")
+}
